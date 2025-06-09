8e096cd63d1d18bbeb877c784b6b7e1e11efafba
babinicz <babinicz@onet.pl>
2025-06-09 21:06:28 +0200

    zmiany radykalne

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  // Mówimy Vite, że korzeń aplikacji frontendowej jest w folderze 'client'
-  root: path.resolve(import.meta.dirname, "client"),
   resolve: {
     alias: {
-      // Teraz alias '@' będzie poprawnie wskazywał na 'client/src'
-      "@": path.resolve(import.meta.dirname, "client", "src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
-  },
-  build: {
-    // Określamy, gdzie ma trafiać gotowa, zbudowana aplikacja
-    outDir: path.resolve(import.meta.dirname, "dist"),
-    emptyOutDir: true,
   },
 });
