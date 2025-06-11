4f0748ef41c4f5f746af3a62c288f28dfbf5f6ed
babinicz <babinicz@onet.pl>
2025-06-11 19:58:01 +0200

    mp3

--- src/sections/AIShowcaseSection.tsx
+++ src/sections/AIShowcaseSection.tsx
@@ -47,7 +47,7 @@ export function AIShowcaseSection() {
   const videoUrlWebm =
     "https://res.cloudinary.com/dxf4a29fl/video/upload/v1749641582/ai-generated-video_dgwkpa.webm";
   const musicUrlMp3 =
-    "https://res.cloudinary.com/dxf4a29fl/video/upload/v1749647612/emotional-romantic-piano-and-strings-trailer-3-357149_bwrdwn.mp3";
+    "https://res.cloudinary.com/dxf4a29fl/video/upload/v1749664577/emotional-romantic-optimized-30s_wq6buu.mp3";
   // ===================================================
 
   const handleTryDemo = (type: string) => {
