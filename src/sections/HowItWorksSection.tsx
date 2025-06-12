23f1ad8953df711ad430be721ebb4b5a533991c2
babinicz <babinicz@onet.pl>
2025-06-12 12:25:06 +0200

    zm

--- src/sections/HowItWorksSection.tsx
+++ src/sections/HowItWorksSection.tsx
@@ -24,10 +24,7 @@ export function HowItWorksSection() {
   ];
 
   return (
-    <section
-      id={t("nav.anchors.howItWorks")}
-      className="py-20 bg-[var(--section-alt-bg)]"
-    >
+    <section id={t("nav.anchors.howItWorks")} className="py-20 ">
       <div className="container mx-auto px-4">
         <motion.div
           className="text-center mb-16"
